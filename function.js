b81a25e58972d92ce1e5f3573da06ce8e6c1e7a0
globito69 <126729841+globito69@users.noreply.github.com>
2023-03-01 21:24:34 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,15 @@
+document.getElementById("boton_nombre").onclick= function() {
+   document.getElementById("demo").innerHTML="Mi nombre es Renzo Arredondo";
+ }
+
+ document.getElementById("direccion").onclick= function() {
+  document.getElementById("demo").innerHTML="Vivo en Gutierrez 513, San Rafael, Mendoza";
+}
+
+document.getElementById("celular").onclick= function() {
+  document.getElementById("demo").innerHTML="Mi celular es 2604385959"}
+
+  document.getElementById("email").onclick= function() {
+    document.getElementById("demo").innerHTML="Mi email es renzo.mau1988@gmail.com"}
+
+
